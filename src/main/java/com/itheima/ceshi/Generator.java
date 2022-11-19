6db012003c9792ada7c58296d5edfce0cba74114
java.java <java.java@com.jar>
2022-11-19 15:29:15 +0800

    hot-fix commit1

--- src/main/java/com/itheima/ceshi/Generator.java
+++ src/main/java/com/itheima/ceshi/Generator.java
@@ -5,6 +5,7 @@ public class Generator {
         System.out.println("hello git!!!");
         System.out.println("hello git 2");
         System.out.println("hello git 3");
+        System.out.println("hello git 4");
 
 
     }
