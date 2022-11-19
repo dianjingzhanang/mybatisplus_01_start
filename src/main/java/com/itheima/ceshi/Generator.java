be2f992433e5febefcf4f559981c3c6d0b47de09
java.java <java.java@com.jar>
2022-11-19 14:36:07 +0800

    second commit

--- src/main/java/com/itheima/ceshi/Generator.java
+++ src/main/java/com/itheima/ceshi/Generator.java
@@ -3,6 +3,9 @@ package com.itheima.ceshi;
 public class Generator {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+
 
     }
 }
