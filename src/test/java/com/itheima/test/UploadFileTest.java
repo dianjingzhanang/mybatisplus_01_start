5a76b33d27e03fcb5b79a0ef5a46b63c5c0c3052
java.java <java.java@com.jar>
2023-03-19 16:34:00 +0800

    使用redis缓存短信验证码和菜品数据

--- src/test/java/com/itheima/test/UploadFileTest.java
+++ src/test/java/com/itheima/test/UploadFileTest.java
@@ -1,13 +1,19 @@
 package com.itheima.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
 
 public class UploadFileTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void test1(){
-        String fileName="erwert.jpg";
-        String substring = fileName.substring(fileName.lastIndexOf("."));
-        System.out.println(substring);
+        //redis,清除所有菜品的缓存数据
+        Set keys = redisTemplate.keys("dish_*");
+        redisTemplate.delete(keys);
     }
 }
